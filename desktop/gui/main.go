9582550eabd33036c230dea89583032bb91c7d96
forpersuit <forpersuit@gmail.com>
2026-06-11 22:36:24 +0800

    Redirect Wails GUI options log output to desktop.log file

--- desktop/gui/main.go
+++ desktop/gui/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"context"
 	"embed"
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/wailsapp/wails/v2"
+	wailslogger "github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
@@ -13,7 +18,57 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
+type FileLogger struct {
+	file *os.File
+}
+
+func NewFileLogger(filePath string) *FileLogger {
+	_ = os.MkdirAll(filepath.Dir(filePath), 0755)
+	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return &FileLogger{}
+	}
+	return &FileLogger{file: f}
+}
+
+func (l *FileLogger) Close() {
+	if l.file != nil {
+		_ = l.file.Close()
+	}
+}
+
+func (l *FileLogger) log(level string, message string) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05.000")
+	line := fmt.Sprintf("[%s] [%s] %s\n", timestamp, level, message)
+	fmt.Print(line)
+	if l.file != nil {
+		_, _ = l.file.WriteString(line)
+	}
+}
+
+func (l *FileLogger) Print(message string)   { l.log("PRINT", message) }
+func (l *FileLogger) Trace(message string)   { l.log("TRACE", message) }
+func (l *FileLogger) Debug(message string)   { l.log("DEBUG", message) }
+func (l *FileLogger) Info(message string)    { l.log("INFO", message) }
+func (l *FileLogger) Warning(message string) { l.log("WARN", message) }
+func (l *FileLogger) Error(message string)   { l.log("ERROR", message) }
+func (l *FileLogger) Fatal(message string)   { l.log("FATAL", message) }
+
+func desktopLogFilePath() string {
+	dir, err := os.UserCacheDir()
+	if err != nil {
+		dir = os.TempDir()
+	}
+	return filepath.Join(dir, "eqrcp", "desktop.log")
+}
+
 func main() {
+	logPath := desktopLogFilePath()
+	fileLogger := NewFileLogger(logPath)
+	defer fileLogger.Close()
+
+	fileLogger.Info("EQT GUI Starting...")
+
 	// Create an instance of the app structure
 	app := NewApp()
 	tray := newTrayController(app)
@@ -63,9 +118,12 @@ func main() {
 		Bind: []interface{}{
 			app,
 		},
+		Logger:   fileLogger,
+		LogLevel: wailslogger.INFO,
 	})
 
 	if err != nil {
+		fileLogger.Fatal(fmt.Sprintf("Wails Run error: %v", err))
 		println("Error:", err.Error())
 	}
 }
